d76011a1551bb52f3a46409e40f8b99c552360d9
changhongyan <745030092@qq.com>
2019-01-21 16:23:37 +0800

    convert to limit poker

--- main.py
+++ main.py
@@ -5,19 +5,16 @@ from randomplayer import RandomPlayer
 from honestplayer import HonestPlayer
 
 
+#TODO:config the config as our wish
+config = setup_config(max_round=10, initial_stack=10000, small_blind_amount=10)
 
-config = setup_config(max_round=100, initial_stack=1000, small_blind_amount=20)
-config.register_player(name="f1", algorithm=FishPlayer())
-config.register_player(name="f2", algorithm=FishPlayer())
-config.register_player(name="f3", algorithm=FishPlayer())
-config.register_player(name="f4", algorithm=FishPlayer())
-config.register_player(name="f5", algorithm=RandomPlayer())
-config.register_player(name="r1", algorithm=RandomPlayer())
-config.register_player(name="r2", algorithm=RandomPlayer())
-config.register_player(name="r3", algorithm=RandomPlayer())
-config.register_player(name="r4", algorithm=RandomPlayer())
-config.register_player(name="r5", algorithm=RandomPlayer())
-config.register_player(name="h1", algorithm=HonestPlayer())
+
+
+config.register_player(name="f1", algorithm=RandomPlayer())
+config.register_player(name="f2", algorithm=RandomPlayer())
+
+
+
 
 
 game_result = start_poker(config, verbose=1)
